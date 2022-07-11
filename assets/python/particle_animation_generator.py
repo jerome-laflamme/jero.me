a941a41a4dc983f0284d741c9c4c27ecfc01a86c
Jerome Laflamme <jerome@laflamme.ws>
2022-07-10 20:22:56 -0400

    Last commit 10 jui 2022

---
+++ assets/python/particle_animation_generator.py
@@ -0,0 +1,14 @@
+
+
+import numbers
+
+
+number_of_particles = 100
+animation_time = 6 / 100
+
+print(animation_time)
+
+# for i in range(number_of_particles):
+    # print(".particle:nth-child({number_of_particles}n + {}) {".format(number_of_particles, i + 1))
+    # print("    -webkit-animation-delay: 0.{}s".format(i))
+    # print("}\n")

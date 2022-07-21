9fd888ee69647c72c7e807c99653307b8832a58b
Jerome <jerome@laflamme.ws>
2022-07-21 10:22:10 -0400

    particule layout changed

--- assets/python/particle_animation_generator.py
+++ assets/python/particle_animation_generator.py
@@ -8,7 +8,7 @@ animation_time = 6 / 100
 
 print(animation_time)
 
-# for i in range(number_of_particles):
-    # print(".particle:nth-child({number_of_particles}n + {}) {".format(number_of_particles, i + 1))
-    # print("    -webkit-animation-delay: 0.{}s".format(i))
-    # print("}\n")
+for i in range(number_of_particles):
+    print(".particle:nth-child({number_of_particles}n + {}) {".format(number_of_particles, i + 1))
+    print("    -webkit-animation-delay: 0.{}s".format(i))
+    print("}\n")

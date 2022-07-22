265d9a344092aaa420339734c8baa5925c5cefc7
Jerome <jerome@laflamme.ws>
2022-07-22 17:15:35 -0400

    started 2nd page + img imports

--- assets/python/particle_animation_generator.py
+++ assets/python/particle_animation_generator.py
@@ -3,12 +3,12 @@
 import numbers
 
 
-number_of_particles = 100
+number_of_particles = 99
 animation_time = 6 / 100
 
 print(animation_time)
 
 for i in range(number_of_particles):
-    print(".particle:nth-child({number_of_particles}n + {}) {".format(number_of_particles, i + 1))
-    print("    -webkit-animation-delay: 0.{}s".format(i))
+    print(".particle:nth-child(99n + {}) ".format(i+1) + "{")
+    print("    -webkit-animation-delay: 0.{}s".format(i + 1))
     print("}\n")
